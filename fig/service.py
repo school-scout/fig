8cdf8aca4376b13db8d6918618a4f4299318f183
Aanand Prasad <aanand.prasad@gmail.com>
2014-05-15 11:02:33 +0100

    Fall back to utf-8 when streaming build output
    Fixes #212.
    
    This is an issue with the binary builds of Fig, which seem to default to
    ASCII encoding when writing to stdout.

--- fig/service.py
+++ fig/service.py
@@ -364,18 +364,18 @@ def stream_output(output, stream):
                 diff = len(lines) - lines[image_id]
             else:
                 lines[image_id] = len(lines)
-                stream.write("\n")
+                write(stream, "\n")
                 diff = 0
 
             if is_terminal:
                 # move cursor up `diff` rows
-                stream.write("%c[%dA" % (27, diff))
+                write(stream, "%c[%dA" % (27, diff))
 
         print_output_event(event, stream, is_terminal)
 
         if 'id' in event and is_terminal:
             # move cursor back down
-            stream.write("%c[%dB" % (27, diff))
+            write(stream, "%c[%dB" % (27, diff))
 
         stream.flush()
 
@@ -389,36 +389,40 @@ def print_output_event(event, stream, is_terminal):
 
     if is_terminal and 'stream' not in event:
         # erase current line
-        stream.write("%c[2K\r" % 27)
+        write(stream, "%c[2K\r" % 27)
         terminator = "\r"
         pass
     elif 'progressDetail' in event:
         return
 
     if 'time' in event:
-        stream.write("[%s] " % event['time'])
+        write(stream, "[%s] " % event['time'])
 
     if 'id' in event:
-        stream.write("%s: " % event['id'])
+        write(stream, "%s: " % event['id'])
 
     if 'from' in event:
-        stream.write("(from %s) " % event['from'])
+        write(stream, "(from %s) " % event['from'])
 
     status = event.get('status', '')
 
     if 'progress' in event:
-        stream.write("%s %s%s" % (status, event['progress'], terminator))
+        write(stream, "%s %s%s" % (status, event['progress'], terminator))
     elif 'progressDetail' in event:
         detail = event['progressDetail']
         if 'current' in detail:
             percentage = float(detail['current']) / float(detail['total']) * 100
-            stream.write('%s (%.1f%%)%s' % (status, percentage, terminator))
+            write(stream, '%s (%.1f%%)%s' % (status, percentage, terminator))
         else:
-            stream.write('%s%s' % (status, terminator))
+            write(stream, '%s%s' % (status, terminator))
     elif 'stream' in event:
-        stream.write("%s%s" % (event['stream'], terminator))
+        write(stream, "%s%s" % (event['stream'], terminator))
     else:
-        stream.write("%s%s\n" % (status, terminator))
+        write(stream, "%s%s\n" % (status, terminator))
+
+
+def write(stream, chunk):
+    stream.write(chunk.encode(stream.encoding or 'utf-8'))
 
 
 NAME_RE = re.compile(r'^([^_]+)_([^_]+)_(run_)?(\d+)$')
